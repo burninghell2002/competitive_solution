9df998ce643c8773acd29e6006695a83b8c95e18
abhishekkumar <lovestory1990@gmail.com>
2020-09-25 18:00:13 +0530

    indenting angryProffesor.js

--- HackerRank/angryProfessor.js
+++ HackerRank/angryProfessor.js
@@ -30,63 +30,65 @@ a: an array of integers representing arrival times
 
 */
 
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 // Complete the angryProfessor function below.
 function angryProfessor(k, a) {
-let count = 0;
-for(let i=0;i<a.length;i++){
-    if(a[i]<=0){
-        count++
+  let count = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] <= 0) {
+      count++;
     }
-}
+  }
 
-return count>=k? 'NO':'YES';
-
+  return count >= k ? "NO" : "YES";
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const t = parseInt(readLine(), 10);
+  const t = parseInt(readLine(), 10);
 
-    for (let tItr = 0; tItr < t; tItr++) {
-        const nk = readLine().split(' ');
+  for (let tItr = 0; tItr < t; tItr++) {
+    const nk = readLine().split(" ");
 
-        const n = parseInt(nk[0], 10);
+    const n = parseInt(nk[0], 10);
 
-        const k = parseInt(nk[1], 10);
+    const k = parseInt(nk[1], 10);
 
-        const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+    const a = readLine()
+      .split(" ")
+      .map((aTemp) => parseInt(aTemp, 10));
 
-        let result = angryProfessor(k, a);
+    let result = angryProfessor(k, a);
 
-        ws.write(result + "\n");
-    }
+    ws.write(result + "\n");
+  }
 
-    ws.end();
+  ws.end();
 }
