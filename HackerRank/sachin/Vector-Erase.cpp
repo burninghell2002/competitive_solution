9160f4ab01d2e21d2a12d67634d021ed89143918
Bharat8211 <62749870+Bharat8211@users.noreply.github.com>
2021-10-30 20:25:31 +0530

    Update Vector-Erase.cpp

--- HackerRank/sachin/Vector-Erase.cpp
+++ HackerRank/sachin/Vector-Erase.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <vector>
 #include <iostream>
+#incliude<map>
 #include <algorithm>
 using namespace std;
 
@@ -22,12 +23,17 @@ int main()
     int q1, q2, q3;
     cin>>q1;
     cin>>q2>>q3;
+    
     v.erase (v.begin()+(q1-1));
+    
     v.erase(v.begin()+q2-1 , v.begin()+q3-1);
+    
     cout << v.size() << endl;
     for(j =0 ; j< v.size() ; j++)
     {
         cout << v.at(j) << " " ;
-      }
+    
+        
+    }
 return 0;
 }
